4c63b117c3440475f14f584b56fbf56a6aa56574
Eric Smith <spacewar@gmail.com>
2017-07-03 19:34:53 -0600

    Put firmware version in status box.

--- statusbox.cpp
+++ statusbox.cpp
@@ -3,24 +3,34 @@
 StatusBox::StatusBox(RadioInterface *radio_interface) : QWidget()
 {
   this->radio_interface = radio_interface;
+  refresh_in_progress = false;
 
   hbox = new QHBoxLayout;
 
-  status_label = new QLabel(tr("RS-HFIQ Status:"));
-  hbox->addWidget(status_label);
-  status_text = new QLineEdit();
-  status_text->setPlaceholderText("Text");
-  status_text->setReadOnly(true);
-  hbox->addWidget(status_text);
+  firmware_label = new QLabel(tr("Firmware:"));
+  hbox->addWidget(firmware_label);
+  firmware_text = new QLineEdit();
+  firmware_text->setPlaceholderText("Text");
+  firmware_text->setReadOnly(true);
+  hbox->addWidget(firmware_text);
   hbox->addStretch();
 
   setLayout(hbox);
+
+  refresh();
 }
 
 StatusBox::~StatusBox()
 {
-  delete status_label;
-  delete status_text;
+  delete firmware_label;
+  delete firmware_text;
 
   delete hbox;
+}
+
+void StatusBox::refresh(void)
+{
+  refresh_in_progress = true;
+  firmware_text->setText(QString::fromStdString(radio_interface->send_command("*W")));
+  refresh_in_progress = false;
 }
--- statusbox.h
+++ statusbox.h
@@ -18,13 +18,17 @@ public:
     StatusBox(RadioInterface *radio_interface);
     ~StatusBox();
 
+    void refresh(void);
+
 private:
     RadioInterface *radio_interface;
 
+    bool refresh_in_progress;
+
     QHBoxLayout *hbox;
 
-    QLabel *status_label;
-    QLineEdit *status_text;
+    QLabel *firmware_label;
+    QLineEdit *firmware_text;
 };
 
 #endif // STATUS_BOX_H
